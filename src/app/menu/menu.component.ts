cd2e7034befb620b4eee6a7dcbfafd7211a6fc53
saswata_mishra <saswata_mishra@persistent.com>
2021-10-17 11:47:50 +0530

    added a menu in home page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  // define array of menu items 
   constructor() { }
 
   ngOnInit(): void {
