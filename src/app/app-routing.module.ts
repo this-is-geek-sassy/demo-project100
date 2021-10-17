da2a1293a56c97ae4f76d77f830cfc46bffe6008
saswata_mishra <saswata_mishra@persistent.com>
2021-10-17 12:21:26 +0530

    madeit an SPA (almost)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventoryComponent } from './inventory/inventory.component';
+import { LoginComponent } from './login/login.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Routing configurations 
+  {
+    path: '',
+    // component: LoginComponent
+    redirectTo: 'login',
+    pathMatch: 'full'
+
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'products',
+    component: ProductlistComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  },
+  {
+    path: 'users',
+    component: UserlistComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
