99cc49f1b41120a243d35402609262f9e03379bc
saswata_mishra <saswata_mishra@persistent.com>
2021-10-18 19:11:34 +0530

    added a simulation to autocomplete feature

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -11,12 +11,18 @@ export class RegistrationComponent implements OnInit {
   
   signupForm!: FormGroup;
   // email = new FormControl('', [Validators.required, Validators.email]);
+  keys = new Array<string>();
 
   constructor() { 
     this.signupForm = new FormGroup({
       email:  new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      code: new FormControl('', [Validators.required, validCode])
+      code: new FormControl('', [Validators.required, validCode]),
+      country: new FormControl('', [Validators.required])
+    })
+    this.signupForm.controls.country.valueChanges.subscribe(newkeyword=>{
+      // Make an http call and fetch all countries with 'keyword'
+      this.keys.push(newkeyword);
     })
   }
 
