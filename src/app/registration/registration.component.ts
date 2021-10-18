d83b7d8e04b0b3e25a442429b855d35e823da0ff
saswata_mishra <saswata_mishra@persistent.com>
2021-10-18 16:49:01 +0530

    made the form validations reactive

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  
+  signupForm!: FormGroup;
+  // email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
+  constructor() { 
+    this.signupForm = new FormGroup({
+      email:  new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 
   ngOnInit(): void {
   }
+  
+  get password() {
+    return this.signupForm.get('password');
+  }
 
+  get email() {
+    return this.signupForm.get('email');
+  }
 }
